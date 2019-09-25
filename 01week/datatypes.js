ad246be0418a48d4d85a9e112f029f1760483f75
unknown <CoreyTyler>
2019-09-24 21:26:01 -0500

    data types

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -1,23 +1,48 @@
-Date(); //Display Date and Time
+console.log("The Current Date and time is", Date()); //This shows the current date
 
-.toString(); //Converts a number to a string
+var num = 12;
 
-Number(''); //Converts a string to a number
+num.toString();
 
-Boolean(); //Takes in different data types and prints out whether its a boolean
+console.log(
+  "The string to the number variable typed into num is:",
+  num.toString()
+); //This will change a number to a string. Code starts on line 3.
 
-console.log(typeof ); //Checking if different data types is a Boolean, Null, Undefined, Number, NaN, String
+var str = "22";
 
-console.log(12 + 12); //Adds two numbers together
+console.log("The number from your string is:", Number(str)); //This code will change a string to a number. Code starts on like 14.
 
-if (x==true && y==true) {
-    console.log("true")
-  } //Program that runs only of both values are true
+var datatype = 12;
 
-if (x==true || y==true) {
-    console.log("true") 
-  } //Program that runs only if one of the values is true
+console.log("The data type entered into the variable is a:", typeof datatype); //This will tell you what type of data type you have when typed into the variable datatype. Code starts on like 16. I have datatype in parintheses but when I save the VS code keeps erasing it.
 
-  if (x!==true && y!==true) {
-    console.log("true")
-  }  //Program that only runs of both calues are not true
+var num1 = 2;
+
+var num2 = 44;
+
+console.log("The two num variables added together are:", num1 + num2); //This is a program that will add the two "num" variables together. Again I tried to put the actual num1 + num2 in parentheses but VS code erases them upon save. code starts on line 20.
+
+x = true;
+
+y = true;
+
+if (x == true && y == true) {
+  console.log("both are true");
+} //This is a program that only runs of both of the variables are true. Code starts on line 26.
+
+x = true;
+
+y = 6;
+
+if (x == true || y == true) {
+  console.log("one is true");
+} //This is a program that will only run if one of the variables is true. Code starts on line 34.
+
+x = 5;
+
+y = 6;
+
+if (x !== true && y !== true) {
+  console.log("both are not true");
+} //This is a program that will only run if both of the variables are not true. Code starts on line 42.
