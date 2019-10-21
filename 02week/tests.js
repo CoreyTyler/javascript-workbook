ae65fc85e179372df13e3a702c8dc388b3bc9459
unknown <CoreyTyler>
2019-10-20 22:23:22 -0500

    added tests

---
+++ 02week/tests.js
@@ -0,0 +1,97 @@
+"use strict";
+
+const assert = require("assert");
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const rockPaperScissors = (hand1, hand2) => {
+  hand1 = hand1.trim().toLowerCase();
+  hand2 = hand2.trim().toLowerCase();
+
+  if (typeof hand1 !== "string" || typeof hand2 !== "string") {
+    return `Must enter "rock", "paper" or "scissors"`;
+  } else if (
+    (hand1 !== "rock" && hand1 !== "paper" && hand1 !== "scissors") ||
+    (hand2 !== "rock" && hand2 !== "paper" && hand2 !== "scissors")
+  ) {
+    return `Must enter "rock", "paper" or "scissors"`;
+  } else if (hand1 === hand2) {
+    return "It's a tie!";
+  } else if (hand1 === "rock" && hand2 === "scissors") {
+    return `Hand 1 wins!`;
+  } else if (hand1 === "paper" && hand2 === "rock") {
+    return `Hand 1 wins!`;
+  } else if (hand1 === "scissors" && hand2 === "paper") {
+    return `Hand 1 wins!`;
+  } else if (hand1 === "scissors" && hand2 === "rock") {
+    return `Hand 2 wins!`;
+  } else if (hand1 === "rock" && hand2 === "paper") {
+    return `Hand 2 wins!`;
+  } else if (hand1 === "paper" && hand2 === "scissors") {
+    return `Hand 2 wins!`;
+  }
+};
+
+function getPrompt() {
+  rl.question("hand1: ", answer1 => {
+    rl.question("hand2: ", answer2 => {
+      console.log(rockPaperScissors(answer1, answer2));
+      getPrompt();
+    });
+  });
+}
+
+// Tests
+
+if (typeof describe === "function") {
+  describe("#rockPaperScissors()", () => {
+    it("should detect a tie", () => {
+      assert.equal(rockPaperScissors("rock", "rock"), "It's a tie!");
+      assert.equal(rockPaperScissors("paper", "paper"), "It's a tie!");
+      assert.equal(rockPaperScissors("scissors", "scissors"), "It's a tie!");
+    });
+    it("should detect which hand won", () => {
+      assert.equal(rockPaperScissors("rock", "paper"), `Hand 2 wins!`);
+      assert.equal(rockPaperScissors("paper", "scissors"), `Hand 2 wins!`);
+      assert.equal(rockPaperScissors("rock", "scissors"), `Hand 1 wins!`);
+    });
+    it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
+      assert.equal(rockPaperScissors("rOcK", " paper "), `Hand 2 wins!`);
+      assert.equal(rockPaperScissors("Paper", "SCISSORS"), `Hand 2 wins!`);
+      assert.equal(rockPaperScissors("rock ", "sCiSsOrs"), `Hand 1 wins!`);
+    });
+    it("should scrub input to only accept rock paper or scissors", () => {
+      assert.equal(
+        rockPaperScissors("hammer", " paper "),
+        `Must enter "rock", "paper" or "scissors"`
+      );
+      assert.equal(
+        rockPaperScissors("Paper", "brick"),
+        `Must enter "rock", "paper" or "scissors"`
+      );
+      assert.equal(
+        rockPaperScissors("ball ", "sCiSsOrs"),
+        `Must enter "rock", "paper" or "scissors"`
+      );
+    });
+    it("must enter string with no numbers", () => {
+      assert.equal(
+        rockPaperScissors("9776", " paper "),
+        `Must enter "rock", "paper" or "scissors"`
+      );
+      assert.equal(
+        rockPaperScissors("Paper", "99999"),
+        `Must enter "rock", "paper" or "scissors"`
+      );
+      assert.equal(
+        rockPaperScissors("4355", "sCiSsOrs"),
+        `Must enter "rock", "paper" or "scissors"`
+      );
+    });
+  });
+} else {
+  getPrompt();
+}
