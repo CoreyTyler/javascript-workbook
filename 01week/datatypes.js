d866177c9e5434da1657991dca27e86a1b9994e1
unknown <CoreyTyler>
2019-12-05 19:14:15 -0600

    JsIntro v1

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -1,1 +1,26 @@
-console.log("Starting Assignment 1");
+//Write a JavaScript program to display the current day and time.
+
+let currentDate = new Date();
+
+console.log("The current date and time is", currentDate);
+
+//Write a JavaScript program to convert a number to a string.
+
+var num = 15;
+var n = num.toString();
+
+console.log("The number is", num);
+console.log("The string representing the number is", n);
+
+//Write a JavaScript program to convert a string to the number.
+
+var string = "222";
+var w = Number(string);
+
+console.log("My 222 string changed to a number is", w);
+
+//Write a JavaScript program that takes in different datatypes and prints out their datatype
+
+console.log("This program will tell you it is a", typeof true);
+
+console.log("This program will tell you it is a", typeof );
