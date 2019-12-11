61fb0c7963d376c3e11db6ab67fd7e43115d8793
unknown <CoreyTyler>
2019-12-10 18:31:02 -0600

    datatypesFinal

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -21,6 +21,38 @@ console.log("My 222 string changed to a number is", w);
 
 //Write a JavaScript program that takes in different datatypes and prints out their datatype
 
-console.log("This program will tell you it is a", typeof true);
+console.log(
+  "This program will tell you the data type of a value using typeof followed by the value"
+);
 
-console.log("This program will tell you it is a", typeof );
+typeof "heyyo";
+
+var num1 = 2;
+
+var num2 = 44;
+
+console.log("The two num variables added together are:", num1 + num2); //This is a program that will add the two "num" variables together.
+
+x = true;
+
+y = true;
+
+if (x == true && y == true) {
+  console.log("both are true");
+} //This is a program that only runs of both of the variables are true.
+
+x = true;
+
+y = 0;
+
+if (x == true || y == true) {
+  console.log("one is true");
+} //This is a program that will only run if one of the variables is true.
+
+x = 5;
+
+y = 6;
+
+if (x !== true && y !== true) {
+  console.log("both are not true");
+} //This is a program that will only run if both of the variables are not true.
