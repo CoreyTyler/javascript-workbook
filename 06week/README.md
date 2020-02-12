288c44ee20f6742408b5cf79c9537ba61f4321ca
unknown <CoreyTyler>
2020-02-12 14:35:19 -0600

    Dodgeball

---
+++ 06week/README.md
@@ -0,0 +1,19 @@
+Code Plan for Dodgeball Checkpoint
+
+When starting dodgeball, I first copied the starter code into my .js file which game me an array of objects of people who I could add to my dodgeball teams.
+
+I first started with making 3 empty arrays that would represent my list of players, my red team and my blue team.
+
+Next I had to make my player classes adding all the attributed I needed for each player in my original array of players objects. as well as making functions that allow players to join the red or the blue team.
+
+Next I extended the classes using the constructor with the required attributes.
+
+Next I had to code a function that responded to the button press when clicking "List of People" and adds the players to an unordered list with a button attached. This Displays the people available in order to make them dodeball players.
+
+Next was adding a function that responds to the click of the make player button and adds selected people and adds them to the list of players list with buttons attached to the name of players using the newplayers object adding whichever attributes are attached to the person in the new constructor.
+
+Next I had to create functions that when you clicked the button associated to add them to the team, adds them to a new UL adding the mascot associated with the team as well as the team color though somehow my team color keeps coming out as undefined and me and my tutor couldnt figure out why.
+
+Next I added a few tests with help from my tutor as well. I need a little more practice with making tests. But I made a few that my code passes.
+
+I didnt have time to pretty it up with CSS but all of the html and js woork.
---
+++ 06week/dodgeball.js
@@ -0,0 +1,256 @@
+const arrOfPeople = [
+  {
+    id: 2,
+    name: "Charles Young",
+    age: 55,
+    skillSet: "welding",
+    placeBorn: "Omaha, Nebraska"
+  },
+  {
+    id: 3,
+    name: "Judy Twilight",
+    age: 35,
+    skillSet: "fishing",
+    placeBorn: "Louisville, Kentucky"
+  },
+  {
+    id: 4,
+    name: "Cynthia Doolittle",
+    age: 20,
+    skillSet: "tic tac toe",
+    placeBorn: "Pawnee, Texas"
+  },
+  {
+    id: 5,
+    name: "John Willouby",
+    age: 28,
+    skillSet: "pipe fitting",
+    placeBorn: "New York, New York"
+  },
+  {
+    id: 6,
+    name: "Stan Honest",
+    age: 20,
+    skillSet: "boom-a-rang throwing",
+    placeBorn: "Perth, Australia"
+  },
+  {
+    id: 7,
+    name: "Mia Watu",
+    age: 17,
+    skillSet: "acrobatics",
+    placeBorn: "Los Angeles, California"
+  },
+  {
+    id: 8,
+    name: "Walter Cole",
+    age: 32,
+    skillSet: "jump rope",
+    placeBorn: "New Orleans, Louisiana"
+  }
+];
+
+const listOfPlayers = [];
+const blueTeam = [];
+const redTeam = [];
+
+class Player {
+  constructor(id, name, age, skillSet, placeBorn) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.skillSet = skillSet;
+    this.placeBorn = placeBorn;
+  }
+  joinRedTeam(redTeam) {
+    this.redTeam = redTeam;
+    player.redTeam.push(this);
+  }
+  joinBlueTeam(blueTeam) {
+    this.blueTeam = blueTeam;
+    player.blueTeam.push(this);
+  }
+}
+
+class Teammate extends Player {
+  constructor(
+    id,
+    name,
+    age,
+    skillSet,
+    placeBorn,
+    canThrowBall,
+    canDodgeBall,
+    hasPaid,
+    isHealthy,
+    yearsExperience,
+    mascot,
+    teamColor
+  ) {
+    super(id, name, age, skillSet, placeBorn);
+    this.canThrowBall = canThrowBall;
+    this.canDodgeBall = canDodgeBall;
+    this.hasPaid = hasPaid;
+    (this.isHealthy = isHealthy),
+      (this.yearsExperience = yearsExperience),
+      (this.mascot = mascot);
+    this.teamColor = teamColor;
+  }
+}
+
+const listPeopleChoices = () => {
+  const listElement = document.getElementById("people");
+  var elm = document.querySelector("button");
+  elm.style.display = "none";
+  arrOfPeople.map(person => {
+    const li = document.createElement("li");
+    const button = document.createElement("button");
+    button.innerHTML = "Make Player";
+    button.addEventListener("click", function() {
+      makePlayer(person.id);
+      listElement.removeChild(li);
+    });
+    li.appendChild(button);
+    li.appendChild(
+      document.createTextNode(person.name + " - " + person.skillSet)
+    );
+    listElement.append(li);
+  });
+};
+
+const makePlayer = id => {
+  const players = document.getElementById("players");
+  const findPlayer = arrOfPeople.find(entry => {
+    return entry.id == id;
+  });
+  console.log(findPlayer);
+  const playerIndex = arrOfPeople.indexOf(findPlayer);
+  console.log(
+    `this is the player index ${playerIndex}. Push this to the "Player list". Remove this from the "list of Players`
+  );
+  const newPlayer = new Player(
+    findPlayer.id,
+    findPlayer.name,
+    findPlayer.age,
+    findPlayer.skillSet,
+    findPlayer.placeBorn,
+    true,
+    true,
+    true,
+    true,
+    4
+  );
+  listOfPlayers.push(newPlayer);
+  arrOfPeople.splice(playerIndex, 1);
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(newPlayer.name + " - "));
+  const blueButton = document.createElement("button");
+  blueButton.innerHTML = "Blue Team";
+  blueButton.addEventListener("click", function() {
+    makeBluePlayer(newPlayer.id);
+    players.removeChild(li);
+  });
+  li.appendChild(blueButton);
+
+  const redButton = document.createElement("button");
+  redButton.innerHTML = "Red Team";
+  redButton.addEventListener("click", function() {
+    makeRedPlayer(newPlayer.id);
+    players.removeChild(li);
+  });
+  li.appendChild(redButton);
+
+  players.append(li);
+};
+const makeBluePlayer = id => {
+  const bluePlayer = document.getElementById("blue");
+  const selectBlue = listOfPlayers.find(function(pick) {
+    return pick.id == id;
+  });
+  const blueIndex = listOfPlayers.indexOf(selectBlue);
+  const newBlue = new Teammate(
+    selectBlue.id,
+    selectBlue.name,
+    selectBlue.age,
+    selectBlue.skillSet,
+    selectBlue.placeBorn,
+    true,
+    true,
+    true,
+    true,
+    45,
+    "Blue Bombers",
+    "blue"
+  );
+  blueTeam.push(newBlue);
+  listOfPlayers.splice(blueIndex, 1);
+
+  const li = document.createElement("li");
+  li.appendChild(
+    document.createTextNode(
+      newBlue.name +
+        "- Mascot = " +
+        newBlue.mascot +
+        " and team color is " +
+        newBlue.color
+    )
+  );
+  bluePlayer.append(li);
+};
+const makeRedPlayer = id => {
+  const redPlayer = document.getElementById("red");
+  const selectRed = listOfPlayers.find(function(pick) {
+    return pick.id == id;
+  });
+  const redIndex = listOfPlayers.indexOf(selectRed);
+  const newRed = new Teammate(
+    selectRed.id,
+    selectRed.name,
+    selectRed.age,
+    selectRed.skillSet,
+    selectRed.placeBorn,
+    true,
+    true,
+    true,
+    true,
+    45,
+    "Red Raiders",
+    "Red"
+  );
+  redTeam.push(newRed);
+  listOfPlayers.splice(redIndex, 1);
+
+  const li = document.createElement("li");
+  li.appendChild(
+    document.createTextNode(
+      newRed.name +
+        "- Mascot = " +
+        newRed.mascot +
+        " and team color is " +
+        newRed.color
+    )
+  );
+  redPlayer.append(li);
+};
+
+let assert = require("assert");
+
+if (typeof describe === "function") {
+  describe("Player", function() {
+    it("should be able to become a red Player", function() {
+      if (listOfPlayers.id == 2 && redTeam.id == 2) {
+        return true;
+      }
+    });
+  });
+  it("should be able to become a player", function() {
+    if (arrOfPeople.id == 2 && listOfPlayer.id == 2) {
+      return true;
+    }
+  });
+  it("should be able to become a blue Player", function() {
+    if (listOfPlayers.id == 2 && blueTeam.id == 2) {
+      return true;
+    }
+  });
+}
